e24de26a7fc7d19d3bfe9a807fa797beedb5bd87
Able <erugalev@objectstyle.com>
2020-01-28 11:01:24 +0300

    + checkbox-custom upd + file-custom add + 3.2 responsive changes

--- src/App.vue
+++ src/App.vue
@@ -73,7 +73,8 @@
 		</nav>
 		<div class="flex flex-col flex-grow xl:w-auto bg-gray-200 pt-20 sm:pt-0 sm:h-screen overflow-y-auto">
 			<div class="container mx-auto py-16 px-5">
-				<h1>Add Subscribers</h1>
+				<h1 class="lg:text-center">Add Subscribers</h1>
+				<p class="lg:text-center mb-6">You can choose to send your email campaigns using your current Email Service Provider or through Ampjar. Either way we’ll help you build and style your campaigns in minutes.</p>
 				<h2>Import Subscribers</h2>
 				<div class="flex flex-wrap items-center justify-between">
 					<p class="sm:w-9/12 pr-3">If you have connected your preferred email service provider and sync your mailing list into Ampjar.</p>
@@ -83,10 +84,14 @@
 				<h2>Import from file</h2>
 				<div class="flex flex-wrap items-center justify-between">
 					<p class="sm:w-9/12 pr-3">You can upload an Excel or CSV file to add your subscribers.</p>
+					<div class="file-custom mb-3 w-auto">
+						<input type="file" class="file-custom-input" id="importFile">
+						<label class="file-custom-label" for="customFile">Choose file</label>
+					</div>
                     <div class="flex flex-wrap items-center justify-between">
-                        <div class="checkbox-custom mb-0  sm:w-9/12 flex pr-3">
-                            <input id="add-subscribers-permission" type="checkbox" checked>
-                            <label for="add-subscribers-permission" class="checkbox-label">
+                        <div class="checkbox-custom lg:mb-0 sm:w-9/12 flex pr-3">
+                            <input id="import-permission" type="checkbox" checked>
+                            <label for="import-permission" class="checkbox-label">
                                 By uploading this database I confirm that I have been given full and proper permission to contact this database for marketing purposes.
                             </label>
                         </div>
@@ -98,9 +103,9 @@
 				<p>Copy and paste your subscribers' information below. Provide their first name, last name, and email address, in that order. Make sure to separate the details of each subscriber with a comma, and place each subscriber on a new line!</p>
 				<textarea name="" id="" cols="30" rows="10" class="mb-6">First Name, Last Name, Email Address, First Name, Last Name, Email Address, First Name, Last Name, Email Address, First Name, Last Name, Email Address</textarea>
 				<div class="flex flex-wrap items-center justify-between">
-                    <div class="checkbox-custom mb-0  sm:w-9/12 flex pr-3">
-                        <input id="add-subscribers-permission" type="checkbox" checked>
-                        <label for="add-subscribers-permission" class="checkbox-label">
+                    <div class="checkbox-custom lg:mb-0 sm:w-9/12 flex pr-3">
+                        <input id="copypaste-permission" type="checkbox" checked>
+                        <label for="copypaste-permission" class="checkbox-label">
                             By uploading this database I confirm that I have been given full and proper permission to contact this database for marketing purposes.
                         </label>
                     </div>
@@ -125,7 +130,7 @@
 					</div>
 				</form>
 				<div class="flex flex-wrap items-center justify-between">
-                    <div class="checkbox-custom mb-0  sm:w-9/12 flex pr-3">
+                    <div class="checkbox-custom lg:mb-0 sm:w-9/12 flex pr-3">
                         <input id="add-subscribers-permission" type="checkbox" checked>
                         <label for="add-subscribers-permission" class="checkbox-label">
                             I confirm that I have been given full and proper permission to contact this person for marketing purposes.
