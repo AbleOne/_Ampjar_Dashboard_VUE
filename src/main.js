00eedf1042e4931a99648e73fdda213107f01eb1
Able <erugalev@objectstyle.com>
2020-02-10 14:33:36 +0300

    + added components styles and markup :    input type range    inpuy type multi_range
    + added views :
       ONB2020-06-QAge.vue
       ONB2020-07-QGender.vue

--- src/main.js
+++ src/main.js
@@ -1,11 +1,56 @@
 import Vue from 'vue';
 import App from './App.vue';
 import 'date-input-polyfill';
+import noUiSlider from 'nouislider';
 
 import './assets/styles/main.css';
+import 'nouislider/distribute/nouislider.min.css'
 
 Vue.config.productionTip = false;
 
+document.addEventListener("DOMContentLoaded", function(event) {
+  //   Input range : Native (+ css-vars)
+  //
+  //   document.documentElement.classList.add('js-range');
+
+  //   const _R = document.querySelector('[type=range]');
+    
+  //   _R.style.setProperty('--levels', (_R.getAttribute('max') - _R.getAttribute('min')) / _R.getAttribute('step'));
+  //   _R.style.setProperty('--current-val', _R.value - 1);
+    
+    
+  //   _R.addEventListener('input', e => {
+  //     _R.style.setProperty('--current-val', _R.value - 1);
+  //   }, false);
+
+
+  //   Input range : NoUISlider
+  //   get `data-` from html
+  function input_range_data ( element, key ) {
+      return element.getAttribute('data-' + key);   
+  }
+
+  // create instance of input_range_multi
+  function createInputRangeMulti(slider) {
+
+    var range_multi_start = JSON.parse(input_range_data(slider, "start"));
+    var range_multi_ranges = JSON.parse(input_range_data(slider, "ranges"));
+    
+    noUiSlider.create(slider, {
+      start: range_multi_start,
+      connect: true,
+      range: range_multi_ranges
+    });
+  }
+
+  var inputs_range_multi = document.getElementsByClassName('input-range-multi');
+
+  for ( var i = 0; i < inputs_range_multi.length; i++ ) {
+    createInputRangeMulti(inputs_range_multi[i])
+  }
+
+});
+  
 new Vue({
   render: h => h(App),
 }).$mount(`#ampjar_dashboard`);
---
+++ src/pages/ONB2020-07-QGender.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="dashboard">
+        <!-- <navigation/> -->
+        <main class="main-content justify-center bg-white py-0">
+            <div class="container self-center">
+                <div class="mx-auto p-5">
+                    <form class="flex flex-col" action="">
+                        <div class="text-center">
+                            <h1 class="mb-4">Most of my current customers identify as:</h1>
+                            <p>Drag the slider to select their gender.</p>
+                        </div>
+                        <label class="hidden" for="customer-gender-now">Customers gender now</label>
+                        <input type="range" min="1" max="5" step="1" value="3" id="customer-gender-now" class="mt-6">
+                        <div class="flex justify-between text-xs text-center mb-20">
+                            <span>Male</span>
+                            <span class="ml-3">Primarily<br>Male</span>
+                            <span class="ml-3">Male and<br>Female</span>
+                            <span class="ml-3">Primarily<br>Female</span>
+                            <span>Female</span>
+                        </div>
+                        <div class="text-center">
+                            <h1 class="mb-4">I’d like to reach more customers who identify as:</h1>
+                            <p>Drag the slider to select the gender of your ideal customers.</p>
+                        </div>
+                        <label class="hidden" for="customer-gender-future">Customers gender future</label>
+                        <input type="range" min="1" max="5" step="1" value="3" id="customer-gender-future" class="mt-6">
+                        <div class="flex justify-between text-xs text-center mb-12">
+                            <span>Male</span>
+                            <span class="ml-3">Primarily<br>Male</span>
+                            <span class="ml-3">Male and<br>Female</span>
+                            <span class="ml-3">Primarily<br>Female</span>
+                            <span>Female</span>
+                        </div>
+                        <button type="submit" class="btn primary w-full sm:w-2/5 mx-auto">Continue</button>
+                    </form>
+                </div>
+            </div>
+        </main>
+    </div>
+</template>
+
+<script>
+import navigation from "../src/components/navigation";
+import dropdown from "../src/components/dropdown";
+import StarRating from "vue-star-rating";
+
+export default {
+  name: `App`,
+  components: {
+    navigation,
+    dropdown,
+    StarRating
+  }
+};
+</script>
